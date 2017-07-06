87a7603f51b68699d71286e0d797a19de6295ca8
swimly <979741120@qq.com>
2017-07-06 18:51:06 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <div id="app" class="app">
+    <scroll>
+      <div slot="content">
+        <ul class="list">
+          <li><a href="#">这是列表</a></li>
+          <li><a href="#">这是列表</a></li>
+          <li><a href="#">这是列表</a></li>
+          <li><a href="#">这是列表</a></li>
+          <li><a href="#">这是列表</a></li>
+          <li><a href="#">这是列表</a></li>
+          <li><a href="#">这是列表</a></li>
+          <li><a href="#">这是列表</a></li>
+          <li><a href="#">这是列表</a></li>
+          <li><a href="#">这是列表</a></li>
+          <li><a href="#">这是列表</a></li>
+          <li><a href="#">这是列表</a></li>
+          <li><a href="#">这是列表</a></li>
+          <li><a href="#">这是列表</a></li>
+          <li><a href="#">这是列表</a></li>
+          <li><a href="#">这是列表</a></li>
+          <li><a href="#">这是列表</a></li>
+          <li><a href="#">这是列表</a></li>
+        </ul>
+      </div>
+    </scroll>
+  </div>
+</template>
+
+<script>
+import scroll from '@/components/Scroll'
+
+export default {
+  name: 'app',
+  components: {
+    scroll
+  }
+}
+</script>
+
+<style>
+html{font-size:calc(3.75vw);height:100%;}
+body{margin:0;height:100%;}
+.app{height:100%;}
+.list{list-style: none;padding:0;margin:0;}
+.list a{display:block;font-size:1.4rem;color:#666;text-decoration:none;padding:0.8rem 1rem;border-bottom:1px dashed #ccc;}
+</style>
---
+++ src/components/Scroll.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="scroll-view">
+    <slot name="refresh"></slot>
+    <div class="content" ref="content">
+      <slot name="content"></slot>
+    </div>
+    <slot name="load">
+
+    </slot>
+  </div>
+</template>
+<script>
+  import Iscroll from 'iscroll/build/iscroll-probe'
+  export default {
+    data () {
+      return {
+        options: {
+          probeType: 1,
+          mouseWheel: true,
+          bounce: true,
+          scrollY: false,
+          scrollbars: false
+        }
+      }
+    },
+    mounted () {
+      const myScroll = new Iscroll(this.$refs.content, this.options)
+      myScroll.on('onScrollMove', this.onScroll)
+      console.log(myScroll.options)
+    },
+    created () {
+    },
+    methods: {
+      onScroll (e) {
+        console.log(e)
+      }
+    }
+  }
+</script>
+<style>
+.scroll-view{overflow:hidden;height:100%;}
+.content{overflow:auto;height:100%;}
+</style>
+
